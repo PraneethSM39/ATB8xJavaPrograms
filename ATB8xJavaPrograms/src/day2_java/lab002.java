9be176e30ed2f7f5e0ce6e25047ae991ad85fc44
Praneeth SM <praneethsm39@gmail.com>
2024-09-06 08:10:49 +0530

    day2 java

---
+++ ATB8xJavaPrograms/src/day2_java/lab002.java
@@ -0,0 +1,14 @@
+package day2_java;
+
+public class lab002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("start to type");
+		System.out.println(2*2);
+		System.out.println(3-2);
+		
+		
+	}
+
+}
